3a4e61af2aae0611f6e1b82a2ff6329b8e692551
Nícolas 'nohaleh' Deçordi <eowfenth.services@gmail.com>
2018-10-18 21:52:53 -0300

    feat: first blood; add conf for Don't Starve Together dedicated server

---
+++ Server/Master/modoverrides.lua
@@ -0,0 +1,8 @@
+return {
+    ["workshop-375850593"] = { enabled = true },
+    ["workshop-378160973"] = { enabled = true },
+    ["workshop-375859599"] = { enabled = true },
+    ["workshop-356930882"] = { enabled = true },
+    ["workshop-356420397"] = { enabled = true },
+    ["workshop-481765222"] = { enabled = true }
+}
